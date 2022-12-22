a84803af1cff0b9e2415b351134eece6e33659e3
srendon34@soy.sena.edu.co <SantiRendon>
2022-12-22 14:12:34 -0500

    UPDATE file main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-// variables de configuracion
-let url = `https://love-calculator.p.rapidapi.com/getPercentage?sname=Alice&fname=John`
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '1039b05c7cmsh2b70a5c173a1baap1e1566jsn69c10d5bc62b',
-		'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com'
-	}
-};
 
 // Variables HTML Elements
 let container = document.getElementById('')
@@ -22,27 +13,30 @@ let inputFn = document.getElementById('fn');
 let inputSn = document.getElementById('sn');
 let buttonSubmit = document.getElementById('btSub')
 
-let valueFn = inputFn.value
-let valueSn = inputSn.value
 
-let fun = (url,options)=>{
-	
-	fetch(url, options)
-		.then(res => res.json())
-		.then(data =>{
-			// console.log(data)
-			
-			fname.innerHTML=`${data.fname}`;
-			sname.innerHTML=`${data.sname}`;
-			percentage.innerHTML=`${data.percentage}%`;
-			result.innerHTML=`${data.result}`;
-		})
-		.catch(err => console.error(err));
-	}
-		
 buttonSubmit.addEventListener('click',(e)=>{
 	e.preventDefault();
-	
-	fun(url,options);
+
+	// variables de configuracion
+	let url = `https://love-calculator.p.rapidapi.com/getPercentage?sname=${Object.values(inputSn.value)}&fname=${inputFn.value}`
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '1039b05c7cmsh2b70a5c173a1baap1e1566jsn69c10d5bc62b',
+			'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com'
+		}
+	};
+
+	fetch(url, options)
+		.then(res => res.json())
+		.then(data => {
+			// console.log(data)
+
+			fname.innerHTML = `${data.fname}`;
+			sname.innerHTML = `${data.sname}`;
+			percentage.innerHTML = `${data.percentage}%`;
+			result.innerHTML = `${data.result}`;
+		})
+		.catch(err => console.error(err));
 
 })
